3738a00d3052410b567e1eeb43dceaf1cac2a2a0
KDRRRT <tatynakudrin@gmail.com>
2022-10-04 21:23:53 +0300

    задания с функциями

---
+++ fun.py
@@ -0,0 +1,39 @@
+# def read_input():
+#    a = int(input())
+#    b = int(input())
+#    c = input()
+#    return a, b, c
+
+
+# def calculate(a, b, c):
+#    if c == "+":
+#        print(a + b)
+#    elif c == "-":
+#       print(a - b)
+#    elif c == "*":
+#        print(a * b)
+#    elif c == "/":
+#       print(a / b)
+
+
+# a, b, c = read_input()
+
+# calculate(a, b, c)
+
+bank_percent = 0.1
+
+
+def read_input():
+    n = int(input())
+    m = int(input())
+    return n, m
+
+
+def vklad(a, b):
+    total_money = n
+    for i in range(m):
+        total_money = total_money + total_money * 0.1
+
+
+n, m = read_input()
+vklad(n, m)
