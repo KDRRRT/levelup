dd95a2c8523ec254e5688d80013dddb3df4f773a
KDRRRT <tatynakudrin@gmail.com>
2022-10-04 22:16:33 +0300

    задание со строками

---
+++ str.py
@@ -0,0 +1,10 @@
+# text = "Интегра́л — одно из важнейших понятий математического анализа"
+# print(text.count(" "))
+n = int(input())
+sum_str = ""
+for i in range(n):
+    sum_str += input()
+    sum_str += " "
+
+
+print(sum_str)
