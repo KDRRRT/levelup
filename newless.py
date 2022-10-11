3e83b5c6b7108df384cdc7bda3415befe4c40328
KDRRRT <tatynakudrin@gmail.com>
2022-10-11 19:19:58 +0300

    задачи 1 и 2 (треугольник и таблица)

---
+++ newless.py
@@ -0,0 +1,22 @@
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# if a == b and a == c and b == c:
+#     print("треугольник равносторонний")
+# elif a == b:
+#     print("треугольник равнобедренный")
+# else:
+#     print("треугольник разносторонний")
+
+#a = 1
+#while a <= 10:
+#    b = 1
+#    while b <= 10:
+#        c = a * b
+#        print(c, end=" ")
+#        b += 1
+#    print(" ")
+#    a += 1
+
+
